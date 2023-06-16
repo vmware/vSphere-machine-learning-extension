3afca2ef96c68b8459efa5104a5a716995641e99
jack <fche@vmware.com>
2023-06-16 09:34:33 +0800

    Update the download URL.

--- examples/llm_llama_deployment/download_models.sh
+++ examples/llm_llama_deployment/download_models.sh
@@ -10,9 +10,18 @@
 MODEL_SIZE="7B"
 mkdir -p $MODEL_SIZE
 
-HTTP_SERVER_URL="http://10.117.4.208:8001"
-echo "Downloading model files from $HTTP_SERVER_URL"
+#
+# This is an example of the http server URL.
+# HTTP_SERVER_URL="http://10.117.7.217:8001/llama"
+
+if [ -z "$HTTP_SERVER_URL" ]
+then
+    echo "  Error: Modify the script and specify the model download URL, then try again."
+    exit 1
+else
+    echo "Downloading model files from $HTTP_SERVER_URL"
+fi
 
 curl --output tokenizer.model "${HTTP_SERVER_URL}/tokenizer.model"
-curl --output "${MODEL_SIZE}/params.json" "${HTTP_SERVER_URL}/params.json"
-curl --output "${MODEL_SIZE}/consolidated.00.pth" "${HTTP_SERVER_URL}/consolidated.00.pth"
+curl --output "${MODEL_SIZE}/params.json" "${HTTP_SERVER_URL}/${MODEL_SIZE}/params.json"
+curl --output "${MODEL_SIZE}/consolidated.00.pth" "${HTTP_SERVER_URL}/${MODEL_SIZE}/consolidated.00.pth"
